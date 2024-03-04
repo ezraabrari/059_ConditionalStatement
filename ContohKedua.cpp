af97ae6be4c7ea6a8bd6f07178d9c116135d89be
Zyaa <52498432+ezraabrari@users.noreply.github.com>
2024-03-04 21:35:32 +0700

    Tugass

---
+++ ContohKedua.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int nBilangan;
+    
+    string status;
+    srand(time(0));
+
+    nBilangan = rand() % 10;
+    cout << "bilangannya adalah = " << nBilangan <<endl;
+    if (nBilangan % 2 == 0){
+        status = "genap";
+    }
+
+    else {
+        status = "ganjil";
+    }
+
+    cout << "statusnya adalah =" << status << endl;
+
+    return 0;
+}
